9add65cec9e6bcf887cc8e893d0eaf2fa7576f29
Volha Pivavarchyk <pivovarchyk@gmail.com>
2021-08-11 14:57:58 +0300

    Correct formating setting. Only used properties

--- Assets/library/js/reusableDynamicContent/reusableDynamicContent.block.js
+++ Assets/library/js/reusableDynamicContent/reusableDynamicContent.block.js
@@ -1,31 +1,26 @@
 export default class ReusableDynamicContentBlock {
-    editor;
+  editor;
 
-    opts;
+  constructor(editor) {
+    this.editor = editor;
+  }
 
-    blockManager;
-
-    constructor(editor, opts = {}) {
-        this.editor = editor;
-        this.opts = opts;
-        this.blockManager = this.editor.BlockManager;
-    }
-
-    addReusableDynamicContentBlock() {
-        this.blockManager.add('reusable-dynamic-content', {
-            label: Mautic.translate('grapesjsbuilder.reusableDynamicContentBlockLabel'),
-            activate: true,
-            select: true,
-            attributes: { class: 'fa fa-cubes' },
-            category: Mautic.translate('grapesjsbuilder.categoryBlockLabel'),
-            content: {
-                type: 'reusable-dynamic-content',
-                content: '<div>{% TWIG_BLOCK %}<br>\r\n' +
-                    '{{ include(\'dc:Meet Adrian\') }}<br>\r\n' +
-                    '{% END_TWIG_BLOCK %}</div>',
-                style: { padding: '10px' },
-                activeOnRender: 1,
-            },
-        });
-    }
+  addReusableDynamicContentBlock() {
+    this.editor.BlockManager.add('reusable-dynamic-content', {
+      label: Mautic.translate('grapesjsbuilder.reusableDynamicContentBlockLabel'),
+      activate: true,
+      select: true,
+      attributes: { class: 'fa fa-cubes' },
+      category: Mautic.translate('grapesjsbuilder.categoryBlockLabel'),
+      content: {
+        type: 'reusable-dynamic-content',
+        content:
+          '<div>{% TWIG_BLOCK %}<br>r\n' +
+          "{{ include('dc:Meet Adrian') }}<br>r\n" +
+          '{% END_TWIG_BLOCK %}</div>',
+        style: { padding: '10px' },
+        activeOnRender: 1,
+      },
+    });
+  }
 }
