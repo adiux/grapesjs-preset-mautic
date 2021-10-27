02699555243218f6c396ad0db481c7202b41fb95
Adrian Schimpf <adrian@idea2.ch>
2021-10-27 19:19:37 +0200

    translation and icon

--- Assets/library/js/reusableDynamicContent/reusableDynamicContent.block.mjml.js
+++ Assets/library/js/reusableDynamicContent/reusableDynamicContent.block.mjml.js
@@ -9,7 +9,7 @@ export default class reusableDynamicContentBlockMjml {
     this.editor.BlockManager.add('reusable-dynamic-content', {
       label: Mautic.translate('grapesjsbuilder.reusableDynamicContentBlockLabel'),
       category: Mautic.translate('grapesjsbuilder.categoryBlockLabel'),
-      attributes: { class: 'fa fa-tag' },
+      attributes: { class: 'fa fa-puzzle-piece' },
       type: 'reusable-dynamic-content',
       style: { padding: '10px' },
       content: `<mj-text data-slot='reusableDynamicContent'>{% TWIG_BLOCK %}{{ include('dc:0') }}{% END_TWIG_BLOCK %}</mj-text>`,
